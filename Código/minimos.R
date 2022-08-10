655cec2a204bc804b1fa7c87a0e10ee96e8a7dec
jorgeorenos <orenosjorge@gmail.com>
2022-08-10 01:06:54 -0600

    subiendo archivos

---
+++ Código/minimos.R
@@ -0,0 +1,17 @@
+# ciclo para encontrar los n valores mínimos
+
+minimos <- function(x, n){
+  
+  mins <- c()
+  
+  for (i in c(1:n)) {
+  
+    mins[i] <- min(x)
+    
+    x <- x[x!=min(x)]
+    
+      
+  } 
+  
+  return(mins)
+}
